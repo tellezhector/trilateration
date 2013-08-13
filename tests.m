0035429de43acca1b8f06fc8162cc26e2ba93d17
htellez <htellez@nearbpo.com>
2013-08-13 00:55:43 -0500

    Improving test suit.

--- tests.m
+++ tests.m
@@ -4,42 +4,30 @@ r1 = 1;
 r2 = 1;
 
 I = find_intersections(P1, P2, r1, r2);
-if (isequal(I, [1 0]))
-	"+ Interseccion en un solo punto, funciona."
-else
-	"Se esperaba [1 0], pero se encontro:"
-	printf("%d", I);
-endif
+assert(I, [1, 0]);
+printf("+ Interseccion en un solo punto, funciona.\n");
 
 r1 = sqrt(2);
 r2 = sqrt(2);
 
+
 I = find_intersections(P1, P2, r1, r2);
-if (isequal(round(I), [1 -1; 1 1]))
-	"+ Interseccion en dos puntos, funciona."
-else
-	"Se esperaba [1 -1; 1 1], pero se encontro:"
-	printf("%f", I);
-endif
+assert(round(I), [1 -1; 1 1]);
+printf("+ Interseccion en dos puntos, funciona.\n");
 
 r1 = 1;
 r2 = 3;
 
 I = find_intersections(P1, P2, r1, r2);
-if (isequal(I, [-1 0]))
-	"+ Interseccion en un punto donde un circulo contiene al otro, funciona."
-else
-	"Se esperaba [-1 0], pero se encontro:"
-	printf("%f", I);
-endif
+assert(I, [-1 0]);
+printf("+ Interseccion en un punto donde un circulo contiene al otro, funciona.\n");
 
 r1 = .5;
 r2 = .5;
 
 I = find_intersections(P1, P2, r1, r2);
-if (isequal(I, zeros(0, 0)))
-	"+ Interseccion nula, funciona."
-else
-	"Se esperaba zeros(0, 0), pero se encontro:"
-	printf("%f", I);
-endif
+assert(I, zeros(0, 0));
+printf("+ Interseccion nula, funciona.\n");
+
+r1 = .5;
+r2 = .5;
