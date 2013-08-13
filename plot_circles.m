a431080cb92a828855f696f4035d6966610e4446
htellez <htellez@nearbpo.com>
2013-08-13 01:01:29 -0500

    Creating function that plots multiple circles.

---
+++ plot_circles.m
@@ -0,0 +1,18 @@
+function void = plot_circles(P, Re)
+    n = size(P, 1);
+
+    for i = 1:n
+        [x, y] = get_circle_points(P(i,:), Re(i));
+        
+        if (i == 1)
+            X=x';
+            Y=y';
+            continue;
+        endif
+        X(:,i) = x';
+        Y(:,i) = y';
+    endfor
+    
+    plot(X,Y);
+    axis("square");
+endfunction
