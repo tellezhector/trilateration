4051afe57fa67da2e29555c8dbc2044bbe6d6ad9
htellez <htellez@nearbpo.com>
2013-08-13 15:58:46 -0500

    Improving gaussian noise generator.

--- add_gaussian_noise.m
+++ add_gaussian_noise.m
@@ -1,5 +1,9 @@
 function Re = add_gaussian_noise(mu, sigma)
-	n	= size(mu, 1);
-	u	= rand(n, 1);
-	Re	= norminv(u, mu, sigma);
+	samples 	= 	100;
+	n 			= 	size(mu, 1);
+	u 			= 	rand(n, samples);
+	mumat		= 	repmat(mu, 1, samples);
+	sigmamat	=	repmat(sigma, 1, samples);
+	quantiles	=	norminv(u, mumat, sigmamat);
+	Re 			= 	sum(quantiles')'/samples;
 endfunction;
