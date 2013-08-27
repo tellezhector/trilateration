fb8551cd088039607839741c2a6289277aa6e89e
htellez <htellez@nearbpo.com>
2013-08-27 04:36:36 -0500

    fixing strongest_weak_intersection_for_separated_circles.

--- strongest_weak_intersection_for_separate_circles.m
+++ strongest_weak_intersection_for_separate_circles.m
@@ -1,7 +1,8 @@
 function P =	strongest_weak_intersection_for_separate_circles(P1, m1, sigma1, P2, m2, sigma2)
 	difference = P2 - P1;
-    [m, dm] = maximum_gaussians_product(0, sigma1, norm(difference)-m2, sigma2);
+    [m, dm] = maximum_gaussians_product(0, sigma1, norm(difference)-(m2 + m1), sigma2);
     
-    P = dm * difference;
-    P = P / norm(difference);   
+    P = (m1 + dm) * difference;
+    P = P / norm(difference);
+    P = P1 + P;
 endfunction
