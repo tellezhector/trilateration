7cc92af5e60e4c79a7db753fcd3bd3b1afcd1304
htellez <htellez@nearbpo.com>
2013-08-27 04:36:20 -0500

    fixing strongest_weak_intersection_for_engulfed_circles.

--- strongest_weak_intersection_for_engulfed_circles.m
+++ strongest_weak_intersection_for_engulfed_circles.m
@@ -1,11 +1,14 @@
 function P = strongest_weak_intersection_for_engulfed_circles(P1, m1, sigma1, P2, m2, sigma2)
 	if(m1 > m2)
         P = strongest_weak_intersection_for_engulfed_circles(P2, m2, sigma2, P1, m1, sigma1);
+        return;
     endif
     
-    [m, dm] = maximum_gaussians_product(0, sigma1, m2-m1, sigma2);
+    difference = P1-P2;    
+    [m, dm] = maximum_gaussians_product(0, sigma1, m2-(m1 + norm(difference)), sigma2);
     
-    difference = P2-P1;
-    P = dm * difference;
-    P = P / norm(difference);    
+    difference = P1-P2;
+    P = (m1 + dm) * difference;
+    P = P / norm(difference);
+    P = P1 + P;
 endfunction
