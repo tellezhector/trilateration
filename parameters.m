8194f9eb1a5b2855c8d8bcf31796531bc88fe90f
htellez <htellez@nearbpo.com>
2013-09-06 13:13:08 -0500

    displaying more information when generating data.

---
+++ parameters.m
@@ -0,0 +1,11 @@
+printf("------------------------------------\n");
+printf("------------------------------------\n");
+printf("------------------------------------\n");
+printf("USED PARAMETERS\n\n");
+n = 15;
+max_coordinate = 100;
+
+printf("n = %d (number of aerials)\n", n);
+printf("max_coordinate = %d (map size is: 2*max_coordinate X 2*max_coordinate)\n", max_coordinate);
+
+printf("------------------------------------\n\n");
---
+++ random_data.m
@@ -0,0 +1,22 @@
+printf("GENERATING RANDOM DATA\n\n");
+
+printf("Generating aerials' positions.\n");
+P = generate_positions(n, max_coordinate);
+
+printf("Generating position to locate.\n");
+x = generate_positions(1, max_coordinate);
+
+printf("Meassuring distances from point to locate.\n");
+R = distances_from_a_point(P, x);
+printf("Max distance: %f \n", max(R));
+printf("Min distance: %f \n", min(R));
+
+printf("Generating random standard deviations.\n");
+sigma = rand(n, 1) * 100;
+printf("Max standard deviation: %f \n", max(sigma));
+printf("Min standard deviation: %f \n", min(sigma));
+
+printf("Adding gaussian noise.\n");
+Re = abs(add_gaussian_noise(R, sigma));
+
+printf("------------------------------------\n\n");
