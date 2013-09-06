21e9a83173d350f30257c62a1457026096661e3e
htellez <htellez@nearbpo.com>
2013-09-06 13:12:18 -0500

    improving random positions generation.

--- generate_positions.m
+++ generate_positions.m
@@ -1,3 +1,3 @@
 function P = generate_positions(number_of_positions, max_coordinate = 100)
-    P = rand(number_of_positions, 2)*100;
+    P = (rand(number_of_positions, 2) - 0.5)*2*max_coordinate;
 endfunction;
