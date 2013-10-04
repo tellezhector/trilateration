0770d5816b8e19e31586ac2b3d827ac9f6990313
htellez <htellez@nearbpo.com>
2013-10-04 12:24:03 -0500

    Parameterizing main algorithm.

--- approach1.m
+++ approach1.m
@@ -1,7 +1,18 @@
 warning("off", "Octave:broadcast")
 
-parameters
-random_data
+printf("------------------------------------\n");
+printf("------------------------------------\n");
+printf("------------------------------------\n");
+printf("USED PARAMETERS\n\n");
+n = 15;
+max_coordinate = 100;
+
+printf("n = %d (number of aerials)\n", n);
+printf("max_coordinate = %d (map size is: 2*max_coordinate X 2*max_coordinate)\n", max_coordinate);
+
+printf("------------------------------------\n\n");
+
+[P, x, R, sigma, Re] = random_data(n, max_coordinate);
 printf("ALGORITHM STARTS\n\n");
 
 printf("Getting relevant points.\n");
