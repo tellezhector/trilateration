2a884ba33629657c7399d95532f6145f24e4943d
htellez <htellez@nearbpo.com>
2013-10-16 13:52:00 -0500

    implementing use of new functions.

--- approach1.m
+++ approach1.m
@@ -5,9 +5,9 @@ printf("------------------------------------\n");
 printf("------------------------------------\n");
 printf("USED PARAMETERS\n\n");
 n = 15;
-max_coordinate = 100;
 sigma_lower_bound = 0;
 sigma_upper_bound = 100;
+max_coordinate = 100;
 
 printf("n = %d (number of aerials)\n", n);
 printf("max_coordinate = %d (map size is: 2*max_coordinate X 2*max_coordinate)\n", max_coordinate);
@@ -15,34 +15,16 @@ printf("Lower bound for standard deviation: %f \n", sigma_lower_bound);
 printf("Upper bound for standard deviation: %f \n", sigma_upper_bound);
 
 printf("------------------------------------\n\n");
-
+printf("GENERATING RANDOM DATA\n\n");
 [P, x, R, sigma, Re] = random_data(n, max_coordinate, sigma_lower_bound, sigma_upper_bound);
+
+printf("------------------------------------\n\n");
 printf("ALGORITHM STARTS\n\n");
 
-printf("Getting relevant points.\n");
-RP = relevant_points(P, Re, sigma);
-printf("%d relevant points found.\n", size(RP, 1));
-
-printf("Getting weights.\n");
-tic;
-W = weights(RP, P, Re, sigma);
-toc;
-
-printf("Filtering most important points.\n");
-[MI, IW] = filter_most_important_points(RP, W, n);
-
-printf("Getting position. \n");
-y = get_position(IW, MI);
+[y, RP, MI] = find_best_approximation(P, Re, sigma);
 
 printf("------------------------------------\n\n");
 printf("PLOTTING RESULTS.\n");
 
-d = norm(x-y);
-printf("Distance from real position: %f.\n", d);
+plot_results(x, y, RP, MI, P, Re);
 
-[X,Y] = poinst_for_circles_plotting(P, Re);
-[s1, s2] = segment(x, y);
-
-plot(X, Y, "5", RP(:,1), RP(:,2), ".b", MI(:,1), MI(:,2), "or", x(1), x(2), "+", y(1), y(2), "x", s1, s2);
-map_size = 4*max_coordinate;
-axis([-map_size, map_size, -map_size, map_size], "square");
