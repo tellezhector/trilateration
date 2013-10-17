a5e53cd22e055dc1905aa99afc20be5db81b55eb
htellez <htellez@nearbpo.com>
2013-10-17 03:31:07 -0500

    Improving weight measurement.

--- weight.m
+++ weight.m
@@ -1,4 +1,5 @@
 function w = weight(rp, P, Re, sigma)
-	Mi = norm(P - rp, 2, 'rows') - Re;
+    rpm = repmat(rp, size(P, 1), 1);
+	Mi = norm(P - rpm, 2, 'rows') - Re;
 	w = prod(normpdf(0, Mi, sigma));
 endfunction
