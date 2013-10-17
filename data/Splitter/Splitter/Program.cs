1af718f69331ddbb3e073f6327dfdc138b0f6ba6
htellez <htellez@nearbpo.com>
2013-10-17 03:29:40 -0500

    Improving splitter and splitter output.

--- data/Splitter/Splitter/Program.cs
+++ data/Splitter/Splitter/Program.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace Splitter
+﻿namespace Splitter
 {
+    using System;
+    using System.Collections.Generic;
     using System.IO;
+    using System.Linq;
     using System.Text.RegularExpressions;
 
     class Program
@@ -20,32 +18,35 @@ namespace Splitter
 
         static void Main(string[] args)
         {
-            string file_name;
+            string fileName;
             if (args.Length > 0)
             {
-                file_name = args[0];
+                fileName = args[0];
             }
             else
             {
-                file_name = "eposition_1.txt";                
+                fileName = "eposition_1.txt";    
             }
 
-            var withoutExtension = file_name.Split('.')[0];
+            var withoutExtension = fileName.Split('.')[0];
             string directoryPath = string.Format("{0}/{1}", BaseDirectory, withoutExtension);
             Directory.CreateDirectory(directoryPath);
-            var something = new StreamReader(BaseDirectory + "/" + file_name);
+            var something = new StreamReader(BaseDirectory + "/" + fileName);
 
             bool dataStarts = false;
             var regex = new Regex("(\\d\\.{0,1}\\d*\\s*){5,6}");
             List<string> current_case = new List<string>();
             string line = string.Empty;
+
             int counter = 0;
+            StopWatch stopWatch = new StopWatch(true);
+            double seconds;
             while ((line = something.ReadLine()) != null)
             {
                 if (regex.IsMatch(line) && !dataStarts)
                 {
                     dataStarts = true;
-                    Console.WriteLine("uray!");
+                    Console.WriteLine("starting!");
                 }
 
                 if (!dataStarts)
@@ -56,38 +57,60 @@ namespace Splitter
                 if (string.IsNullOrWhiteSpace(line))
                 {
                     counter++;
-                    SaveCase(current_case, file_name, directoryPath);
+                    if (counter % 1000 == 0)
+                    {
+                        seconds = stopWatch.Seconds;
+                        Console.WriteLine("{0} files writen. In {1} seconds.", counter, seconds); 
+                        Console.WriteLine("Current rate {0} files/second.", counter / seconds);
+                    }
+
+                    SaveCase(current_case, withoutExtension, directoryPath);
                     current_case = new List<string>();
                     continue;
                 }
 
                 current_case.Add(line);
             }
+
+            Console.WriteLine("Finished!");
+            seconds = stopWatch.Seconds;
+            Console.WriteLine("{0} files writen. In {1} seconds.", counter, seconds); 
         }
 
-        private static void SaveCase(List<string> currentCase, string fileName, string directoryPath)
+        private static void SaveCase(List<string> currentCase, string prefix, string directoryPath)
         {
             currentCase.Remove(currentCase[1]);
             int nodeId = GetNodeId(currentCase.First());
-            File.WriteAllLines(Path(nodeId, fileName, directoryPath), currentCase);
+            string path = Path(nodeId, prefix, directoryPath);
+            var asdf = new StreamWriter(path);
+
+            foreach (var line in currentCase)
+            {
+                asdf.WriteLine(line);                
+            }
+            
+            asdf.Flush();
+            asdf.Dispose();
         }
 
-        private static string Path(int nodeId, string fileName, string directoryPath)
+        private static string Path(int nodeId, string prefix, string directoryPath)
         {
-            int counter = 1;
-            string path = Path(nodeId, fileName, directoryPath, counter);
-            while (File.Exists(path))
+            if (!_idCounter.ContainsKey(nodeId))
             {
-                counter++;
-                path = Path(nodeId, fileName, directoryPath, counter);
+                _idCounter.Add(nodeId, 1);
             }
 
+            int counter = _idCounter[nodeId]++;
+
+            string path = Path(nodeId, prefix, directoryPath, counter);
             return path;
         }
 
-        private static string Path(int nodeId, string fileName, string directoryPath, int counter)
+        private static Dictionary<int, int> _idCounter = new Dictionary<int, int>(); 
+
+        private static string Path(int nodeId, string prefix, string directoryPath, int counter)
         {
-            var path = string.Format("{0}/{1}_Node{2}_{3}.txt", directoryPath, fileName, nodeId, counter);
+            var path = string.Format("{0}/{1}_Node{2}_{3}.txt", directoryPath, prefix, nodeId, counter);
             return path;
         }
 
---
+++ data/Splitter/Splitter/StopWatch.cs
@@ -0,0 +1,185 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="StopWatch.cs" company="NearBPO">
+//   copyrighttext
+// </copyright>
+// <author>
+// Hector Tellez
+// </author>
+// <summary>
+//   The cronometro.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Splitter
+{
+    using System;
+
+    /// <summary>
+    /// The cronometro.
+    /// </summary>
+    public class StopWatch
+    {
+        /// <summary>
+        /// The _iniciado.
+        /// </summary>
+        private bool _iniciado;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="StopWatch"/> class.
+        /// </summary>
+        public StopWatch()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="StopWatch"/> class.
+        /// </summary>
+        /// <param name="start">
+        /// The start.
+        /// </param>
+        public StopWatch(bool start)
+        {
+            if (start)
+            {
+                Empezar();
+            }
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether Corriendo.
+        /// </summary>
+        public bool Corriendo { get; private set; }
+
+        /// <summary>
+        /// Gets Ticks.
+        /// </summary>
+        /// <exception cref="NotSupportedException">
+        /// Si el cronómetro no se encuentra corriendo, se lanza una excepción.
+        /// </exception>
+        public long Ticks
+        {
+            get
+            {
+                long total;
+                if (!_iniciado)
+                {
+                    throw new NotSupportedException("Este cronómetro nunca comenzó a correr.");
+                }
+
+                if (Corriendo)
+                {
+                    total = DateTime.Now.Ticks - Inicial.Ticks;
+                }
+                else
+                {
+                    total = Final.Ticks - Inicial.Ticks;
+                }
+
+                return total;
+            }
+        }
+
+        /// <summary>
+        /// Gets Minutos.
+        /// </summary>
+        public double Minutos
+        {
+            get
+            {
+                return Math.Round(Milisegundos / 60000, 2);
+            }
+        }
+
+        /// <summary>
+        /// Gets Segundos.
+        /// </summary>
+        public double Seconds
+        {
+            get
+            {
+                return Math.Round(Milisegundos / 1000, 2);
+            }
+        }
+
+        /// <summary>
+        /// Gets Milisegundos.
+        /// </summary>
+        public double Milisegundos
+        {
+            get
+            {
+                return (double)Ticks / 10000;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets Inicial.
+        /// </summary>
+        private DateTime Inicial { get; set; }
+
+        /// <summary>
+        /// Gets or sets Final.
+        /// </summary>
+        private DateTime Final { get; set; }
+
+        /// <summary>
+        /// The empezar.
+        /// </summary>
+        /// <exception cref="NotSupportedException">
+        /// Si se intenta empezar un cronómetro que ya se encontraba corriendo, se lanza una excepción.
+        /// </exception>
+        public void Empezar()
+        {
+            if (Corriendo)
+            {
+                throw new NotSupportedException("Este cronómetro ya se encontraba corriendo.");
+            }
+
+            Inicial = DateTime.Now;
+
+            _iniciado = true;
+            Corriendo = true;
+        }
+
+        /// <summary>
+        /// The detener.
+        /// </summary>
+        /// <exception cref="NotSupportedException">
+        /// Si el cronómetro no se encontraba corriendo y se intenta detener, se lanza una excepción.
+        /// </exception>
+        public void Detener()
+        {
+            if (!Corriendo)
+            {
+                throw new NotSupportedException("Este cronómetro no había empezado a correr. No se puede detener un cronómetro que no esté corriendo.");
+            }
+
+            Final = DateTime.Now;
+
+            Corriendo = false;
+        }
+
+        /// <summary>
+        /// The reiniciar.
+        /// </summary>
+        public void Reiniciar()
+        {
+            lock (this)
+            {
+                Corriendo = false;
+                _iniciado = false;
+
+                Empezar();
+            }
+        }
+
+        /// <summary>
+        /// The continuar.
+        /// </summary>
+        public void Continuar()
+        {
+            Corriendo = true;
+            Final = new DateTime();
+        }
+    }
+}
