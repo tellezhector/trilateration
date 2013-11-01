82b92872bb6826ba77121de25ebe31539a11949a
htellez <htellez@nearbpo.com>
2013-11-01 03:16:39 -0600

    moving data retriever.

--- compare.m
+++ compare.m
@@ -1,27 +1,17 @@
 function void = compare(path)
-	data = dlmread([path], " ");
-	result = data(1, :);
-	aerials_data = data(2:end, :);
-	
-	real_position = result(3:4);
-	z = result(5:6);
-	time = data(1);
-	
-	P = aerials_data(:, 2:3);
-	Re = aerials_data(:, 4);
-	sigma = aerials_data(:, 5);
+	[real_position, estimate, time, P, Re, sigma] = load_comparison_data(path)
 	
 	printf("------------------------------------\n\n");
 	printf("ALGORITHM STARTS.\n");
 
 	tic;
-	[y, RP, MI] = find_best_approximation(P, Re, sigma);
+	[our_estimate, RP, MI] = find_best_approximation(P, Re, sigma);
 	our_time = toc;
 	
 	printf("------------------------------------\n\n");
 	printf("PLOTTING RESULTS.\n");
 
-	plot_comparison_results(real_position, y, z, RP, MI, P, Re, our_time, time);
+	plot_comparison_results(real_position, our_estimate, estimate, RP, MI, P, Re, our_time, time);
 	printf("------------------------------------\n\n");
 	printf("------------------------------------\n\n");
 endfunction
---
+++ load_comparison_data.m
@@ -0,0 +1,13 @@
+function [real_position, estimate, time, P, Re, sigma] = load_comparison_data(path)
+	data = dlmread(path, " ");
+	summary = data(1, :);
+	aerials_data = data(2:end, :);
+	
+	real_position = summary(3:4);
+	estimate = summary(5:6);
+	time = data(1);
+	
+	P = aerials_data(:, 2:3);
+	Re = aerials_data(:, 4);
+	sigma = aerials_data(:, 5);
+endfunction
