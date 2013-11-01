1793b4e4a897fdcd699b03a14e031f074786557a
htellez <htellez@nearbpo.com>
2013-11-01 04:03:21 -0600

    simplifing find_best_approximation for performance purposes.

--- find_best_approximation.m
+++ find_best_approximation.m
@@ -1,16 +1,6 @@
 function [y, RP, MI] = find_best_approximation(P, Re, sigma)
-	printf("Getting relevant points.\n");
 	RP = relevant_points(P, Re, sigma);
-	printf("%d relevant points found.\n", size(RP, 1));
-
-	printf("Getting weights.\n");
-	tic;
 	W = weights(RP, P, Re, sigma);
-	toc;
-
-	printf("Filtering most important points.\n");
 	[MI, IW] = filter_most_important_points(RP, W, size(P, 1));
-
-	printf("Getting position. \n");
 	y = get_position(IW, MI);
 endfunction
