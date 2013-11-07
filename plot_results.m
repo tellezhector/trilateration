e75b739f571a64d61d8c74f08844c2da44c4de74
htellez <htellez@nearbpo.com>
2013-11-07 05:43:38 -0600

    Improving results plotting.

--- plot_results.m
+++ plot_results.m
@@ -6,6 +6,9 @@ function void = plot_results(x, y, RP, MI, P, Re)
 	[s1, s2] = segment(x, y);
 
 	plot(X, Y, "5", RP(:,1), RP(:,2), ".b", MI(:,1), MI(:,2), "or", x(1), x(2), "+", y(1), y(2), "x", s1, s2);
-	map_size = max(max(max(abs(X), abs(Y), 1)));
-	axis([-map_size, map_size, -map_size, map_size], "square");
+	x_max = max(max(X)');
+	x_min = min(min(X)');
+	y_max = max(max(Y)');
+	y_min = min(min(Y)');
+	axis([x_min, x_max, y_min, y_max], "square");
 endfunction
