1047e9140008c9ad588cabc6cfaabfd06c4603eb
htellez <htellez@nearbpo.com>
2013-11-14 01:11:30 -0600

    Changing function name.

--- find_best_approximation.m
+++ find_best_approximation.m
@@ -1,4 +1,4 @@
 function [y, MI, RP] = find_best_approximation(P, Re, sigma)
 	[MI, IW, RP] = most_important_points(P, Re, sigma);
-	y = get_position_approach_1(IW, MI);
+	y = get_position_approach_weighted_mean(IW, MI);
 endfunction
--- get_position_approach_1.m
+++
@@ -1,4 +0,0 @@
-function y = get_position_approach_1(W, RP)
-    weighted    =   RP .* [W W];
-    y           =   sum(weighted, 1)/sum(W);
-endfunction
